05ce4bf8278bcf26a5e449368109bc78feaf698d
buzzfps <gabrielzontabatista@gmail.com>
2023-08-03 11:27:03 -0300

    restaurante3des

--- api/src/controllers/motoboy.js
+++ api/src/controllers/motoboy.js
@@ -6,4 +6,15 @@ const read = async (req, res) => {
     return res.json(motoboys);
 }
 
-module.exports = { read };
+const create = async (req, res) => {
+    const data = req.body;
+    const motoboy = await prisma.motoboy.create({
+        data: data
+    });
+    return res.status(201).json(motoboy).end();
+}
+
+module.exports = {
+    read,
+    create
+};
