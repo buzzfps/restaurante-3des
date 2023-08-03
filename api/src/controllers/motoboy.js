eeb4b2630c6fbb611d6f0af551cb46cd72bde5e0
buzzfps <gabrielzontabatista@gmail.com>
2023-08-03 11:13:35 -0300

    restaurante3des

--- api/src/controllers/motoboy.js
+++ api/src/controllers/motoboy.js
@@ -1,53 +1,9 @@
-const con = require('../DAO/connect')
-const Motoboy = require('../models/motoboy')
+const { PrismaClient } = require('@prisma/client');
+const prisma = new PrismaClient();
 
-const modelarLista = (lista) => {
-    for (i = 0; i < lista.length; i++)
-        lista[i] = new Motoboy(lista[i])
-    return lista
+const read = async (req, res) => {
+    const motoboys = await prisma.motoboy.findMany();
+    return res.json(motoboys);
 }
 
-const cadastrar = (req, res) => {
-    con.query(new Motoboy(req.body).create(), (err, result) => {
-        if (err == null)
-            res.redirect('/')
-        else
-            res.status(500).send(err)
-
-    })
-}
-
-const listar = (req, res) => {
-    let motoboy = new Motoboy(req.params)
-    con.query(motoboy.listar(), (err, result) => {
-        if (err == null) {
-            res.json(modelarLista(result)).end()
-        }
-    })
-}
-
-const alterar = (req, res) => {
-    let motoboy = new Motoboy(req.body)
-    con.query(motoboy.alterar(), (err, result) => {
-        if (result.affectedRows > 0)
-            res.status(202).end()
-        else
-            res.status(404).end()
-    })
-}
-
-const excluir = (req, res) => {
-    con.query(new Motoboy(req.params).delete(), (err, result) => {
-        if (result.affectedRows > 0)
-            res.redirect('/')
-        else
-            res.status(500).send(err)
-    })
-}
-
-module.exports = {
-    cadastrar,
-    listar,
-    alterar,
-    excluir
-}
+module.exports = { read };
