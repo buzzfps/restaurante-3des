ad1f2fe4fc0bdc942c98b67a72c63b31c5031574
buzzfps <gabrielzontabatista@gmail.com>
2023-08-10 08:40:26 -0300

    restaurante3des

---
+++ appmotoboy/src/components/itens/index.js
@@ -0,0 +1,17 @@
+import { View, Text, FlatList } from 'react-native';
+import styles from './styles'
+
+export default function MotoboyPedidos({ item }) {
+    return (
+        <FlatList
+            data={item}
+            renderItem={({ item }) => (
+                <View style={styles.pedido}>
+                    <Text style={styles.text}>Nome: {item.nome}</Text>
+                    <Text style={styles.text}>Placa: {item.motoboy.placa}</Text>
+                    <Text style={styles.text}>Modelo: {item.motoboy.modelo}</Text>
+                </View>
+            )}
+        />
+    )
+}
---
+++ appmotoboy/src/screens/motoboy.js
@@ -0,0 +1,68 @@
+import React, { useState, useEffect } from 'react';
+import { View, Text, FlatList, TouchableOpacity } from 'react-native';
+import styles from './styles'
+import MotoboyPedidos from '../components/itens/index';
+
+const EntregaScreen = ({ navigation }) => {
+
+    const uri = 'http://localhost:3000/motoboy';
+    const [pedidos, setPedidos] = useState([]);
+
+    useEffect(() => {
+        fetch(uri + '/cozinha', { method: 'GET' })
+            .then(response => response.json())
+            .then(data => {
+                setPedidos(data);
+            });
+    }, []);
+
+    const concluirPedido = (id) => {
+        const corpo = {
+            id: id,
+            dataCozinha: new Date()
+        }
+
+        const options = {
+            method: 'PUT',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(corpo)
+        };
+
+        fetch(uri, options)
+            .then(resp => resp.status)
+            .then(data => {
+                if (data = 202) {
+                    navigation.navigate('EntregaScreen')
+                } else {
+                    alert('Erro ao concluir pedido!')
+                }
+            });
+    }
+
+    return (
+        <View style={styles.container}>
+            <FlatList
+                data={pedidos}
+                keyExtractor={item => item.id.toString()}
+                renderItem={({ item }) => (
+                    <View style={styles.pedido}>
+                        <Text style={styles.text}>Id: {item.id}</Text>
+                        <Text style={styles.text}>
+                            Data: {item.dataPedido.toString().slice(0, 10) + " "}
+                            Hora: {item.dataPedido.toString().slice(11, 16)}
+                        </Text>
+                        <MotoboyPedidos item={item.itens} />
+                        <TouchableOpacity
+                            style={styles.button}
+                            onPress={() => concluirPedido(item.id)}
+                        >
+                            <Text style={styles.buttonText}>Concluído</Text>
+                        </TouchableOpacity>
+                    </View>
+                )}
+            />
+        </View>
+    );
+}
+
+export default PedidosScreen;
